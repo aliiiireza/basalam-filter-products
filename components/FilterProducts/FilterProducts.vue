28d656ab7b57c96edcbdf5746ef4f7ab2ab96764
Alireza Abdollahi <mra.abdollahi@gmail.com>
2022-03-04 20:39:09 +0330

    feat: add switch button

--- components/FilterProducts/FilterProducts.vue
+++ components/FilterProducts/FilterProducts.vue
@@ -11,26 +11,55 @@
     </div>
 
     <div class="card mb-3">
-      <div class="card-body">ارسال رایگان</div>
+      <div class="card-body p-2">
+        <u-switch v-model="filters.freeShipping" label="ارسال رایگان" />
+      </div>
     </div>
 
     <div class="card mb-3">
-      <div class="card-body">فقط کالاهای موجود</div>
+      <div class="card-body p-2">
+        <u-switch v-model="filters.isExists" label="فقط کالاهای موجود" />
+      </div>
     </div>
 
     <div class="card mb-3">
-      <div class="card-body">آماده ارسال</div>
+      <div class="card-body p-2">
+        <u-switch v-model="filters.isReady" label="آماده ارسال" />
+      </div>
     </div>
 
     <div class="card mb-3">
-      <div class="card-body">تخفیف دار ها</div>
+      <div class="card-body p-2">
+        <u-switch v-model="filters.hasDiscount" label="تخفیف دار ها" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import USwitch from '~/components/UiKit/USwitch/USwitch'
 export default {
   name: 'FilterProducts',
+  components: {
+    USwitch,
+  },
+  props: {
+    filter: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      filters: {
+        freeShipping: false, // فیلتر محصولات ارسال رایگان
+        isExists: false, // فیلتر محصولات موجود
+        isReady: false, //  فیلتر محصولات آماده ارسال
+        hasDiscount: false, // فیلتر محصولات تخفیف‌دار
+        namedTags: [],
+      },
+    }
+  },
 }
 </script>
 
---
+++ components/UiKit/USwitch/USwitch.vue
@@ -0,0 +1,38 @@
+<template>
+  <label class="u-switch">
+    <input v-model="selfValue" type="checkbox" class="u-switch__input" />
+    <span v-if="label !== ''" class="u-switch__label">{{ label }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'USwitch',
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      selfValue: this.value,
+    }
+  },
+  watch: {
+    selfValue: {
+      handler(value) {
+        this.$emit('input', value)
+      },
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'USwitch';
+</style>
