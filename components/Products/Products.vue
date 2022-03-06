5a81dd12c50ecf4b233463f3a9beba343bedad35
Alireza Abdollahi <mra.abdollahi@gmail.com>
2022-03-06 09:51:55 +0330

    refactor: fetch products

--- components/Products/Products.vue
+++ components/Products/Products.vue
@@ -5,7 +5,7 @@
       :key="product.id + '-' + index"
       :product="product"
     />
-    <div class="col-12">
+    <div v-if="loading || total === 0" class="col-12">
       <div class="card">
         <div class="card-body">
           <template v-if="loading"> در حال بارگزاری... </template>
@@ -45,16 +45,10 @@ export default {
       total: 0,
     }
   },
-  fetch() {
-    this.loading = true
-    this.getProducts()
-      .then((response) => {
-        this.products = response.data.products
-        this.total = response.meta.count
-      })
-      .finally(() => {
-        this.loading = false
-      })
+  async fetch() {
+    const { products, total } = await this.getProducts()
+    this.total = total
+    this.products = products
   },
   watch: {
     payload: {
@@ -74,26 +68,34 @@ export default {
   },
   methods: {
     getProducts() {
-      const url = '/api/v2.0/product/search'
-      return this.request(url, 'post', {
-        q: this.payload.keyword !== undefined ? this.payload.keyword : '',
-        from: this.from,
-        size: this.size,
-        ...this.payload,
+      this.loading = true
+      return new Promise((resolve) => {
+        const url = '/api/v2.0/product/search'
+        let products = []
+        let total = 0
+
+        this.request(url, 'post', {
+          q: this.payload.keyword !== undefined ? this.payload.keyword : '',
+          from: this.from,
+          size: this.size,
+          ...this.payload,
+        })
+          .then((response) => {
+            products = response.data.products
+            total = response.data.meta.count
+          })
+          .finally(() => {
+            this.loading = false
+            resolve({ products, total })
+          })
       })
     },
-    infiniteHandle($state) {
+    async infiniteHandle($state) {
       this.from += this.size
-      this.loading = true
-      this.getProducts()
-        .then((response) => {
-          this.products = [...this.products, ...response.data.products]
-          if (response.data.products.length === 0) $state.complete()
-          else $state.loaded()
-        })
-        .finally(() => {
-          this.loading = false
-        })
+      const { products } = await this.getProducts()
+      this.products = [...this.products, ...products]
+      if (products.length === 0) $state.complete()
+      else $state.loaded()
     },
   },
 }
